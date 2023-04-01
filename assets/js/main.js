e1ab95ca3038a55f492d36411163fc4f429c511d
q0ney <propassiol@gmail.com>
2023-04-01 19:54:39 +0200

    Added login page

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const forms = document.querySelector(".forms"),
+pwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  
+  pwFields.forEach(password => {
+      if(password.type === "password"){
+          password.type = "text";
+          eyeIcon.classList.replace("la-eye", "la-eye-slash");
+          return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("la-eye-slash", "la-eye");
+  })
+  
+})
+}) 
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        forms.classList.toggle("show-signup")
+    })
+})
